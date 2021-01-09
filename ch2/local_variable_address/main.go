02115aff13032e2abd867a594c31f8130d8538d3
Li Zhuo <lizhuo99@foxmail.com>
2021-01-09 15:59:42 +0800

    Returning the address of local variable is perfectly safe.

---
+++ ch2/local_variable_address/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("main: Pointer that f returned: %x\n", f())
+}
+
+//create a local variable a return it's address
+func f() *int {
+	var i int
+	fmt.Printf("f: Address of local variable: %x\n", &i)
+	return &i
+}
