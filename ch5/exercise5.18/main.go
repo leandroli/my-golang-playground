f2bb9536664a73b1b2c048898b9040139fbf213f
Li Zhuo <lizhuo99@foxmail.com>
2021-01-22 18:56:50 +0800

    update exercise5.18

---
+++ ch5/exercise5.18/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"os"
+	"path"
+)
+
+func fetch(url string) (filename string, n int64, err error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", 0, err
+	}
+	defer resp.Body.Close()
+
+	local := path.Base(resp.Request.URL.Path)
+	if local == "/" {
+		local = "index.html"
+	}
+	f, err := os.Create(local)
+	if err != nil {
+		return "", 0, err
+	}
+	n, err = io.Copy(f, resp.Body)
+	defer func() {
+		if err == nil {
+			err = f.Close()
+		}
+	}()
+	return local, n, err
+}
