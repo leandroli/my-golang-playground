cc7d17c2ea1aa1f79c587d7e1460b1d669c03a2f
Li Zhuo <lizhuo99@foxmail.com>
2021-01-23 21:19:51 +0800

    update exercise6.2

---
+++ ch6/exercise6.2/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+)
+
+type IntSet struct {
+	words []uint64
+}
+
+func (s *IntSet) Add(x int) {
+	word, bit := x/64, uint(x%64)
+	for word >= len(s.words) {
+		s.words = append(s.words, 0)
+	}
+	s.words[word] |= 1 << bit
+}
+
+func (s *IntSet) AddAll(xs ...int) {
+	for _, x := range xs {
+		s.Add(x)
+	}
+}
+
+func (s *IntSet) String() string {
+	var buf bytes.Buffer
+	buf.WriteByte('{')
+	for i, word := range s.words {
+		if word == 0 {
+			continue
+		}
+		for j := 0; j < 64; j++ {
+			if word&(1<<uint(j)) != 0 {
+				if buf.Len() > len("{") {
+					buf.WriteByte(' ')
+				}
+				fmt.Fprintf(&buf, "%d", 64*i+j)
+			}
+		}
+	}
+	buf.WriteByte('}')
+	return buf.String()
+}
+
+func main() {
+	var x IntSet
+	x.AddAll(1, 3, 4, 6)
+	fmt.Println(x.String())
+}
