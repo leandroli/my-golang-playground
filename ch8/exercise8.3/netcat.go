8a7b2aece0b1ce89597ff5bb8a6259a74c62f0a1
Li Zhuo <lizhuo99@foxmail.com>
2021-01-30 23:42:16 +0800

    update exercise 8.3

---
+++ ch8/exercise8.3/netcat.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+//!+
+func main() {
+	conn, err := net.Dial("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	done := make(chan struct{})
+	go func() {
+		io.Copy(os.Stdout, conn) // NOTE: ignoring errors
+		log.Println("done")
+		done <- struct{}{} // signal the main goroutine
+	}()
+	mustCopy(conn, os.Stdin)
+	connTCP, ok := conn.(*net.TCPConn)
+	if !ok {
+		log.Fatal(fmt.Errorf("not using a tcp connection"))
+	}
+	connTCP.CloseWrite()
+	<-done // wait for background goroutine to finish
+}
+
+//!-
+
+func mustCopy(dst io.Writer, src io.Reader) {
+	if _, err := io.Copy(dst, src); err != nil {
+		log.Fatal(err)
+	}
+}
